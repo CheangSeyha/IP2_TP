8be6e23f7ad5a6cf9150d1d64c6b03a9528171d5
Axgust22 <cheangseyha2208@gmail.com>
2026-06-11 21:31:34 +0700

    TP6 is done

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,32 +1,41 @@
 import { test, expect } from '@playwright/test';
 
-test('Sauce Demo homepage loads', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await expect(page).toHaveTitle(/Swag Labs/);
-});
-
 test('login with valid credentials', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
-  // Locating elements using user-centric placeholder attributes
   await page.getByPlaceholder('Username').fill('standard_user');
   await page.getByPlaceholder('Password').fill('secret_sauce');
   await page.getByRole('button', { name: /login/i }).click();
 
-  // Assert successful transition into inventory page
   await expect(page).toHaveURL(/inventory.html/);
   await expect(page.getByText('Products')).toBeVisible();
 });
 
-test('login fails with wrong password', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+// Challenge A
+test('login fails with locked out user error', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
 
+    await page.getByPlaceholder('Username').fill('locked_out_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: /login/i }).click();
+
+    const errorContainer = page.locator('[data-test="error"]');
+    await expect(errorContainer).toBeVisible();
+    await expect(errorContainer).toContainText('Sorry, this user has been locked out.');
+});
+
+
+// Challenge C
+test('successful logout from dashboard menu', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
   await page.getByPlaceholder('Username').fill('standard_user');
-  await page.getByPlaceholder('Password').fill('wrong_password');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
   await page.getByRole('button', { name: /login/i }).click();
 
-  // Validate the explicit data-test error element behavior
-  await expect(page.locator('[data-test="error"]')).toContainText(
-    'Username and password do not match any user in this service'
-  );
+  await page.getByRole('button', { name: /open menu/i }).click();
+
+  await page.getByRole('link', { name: /logout/i }).click();
+
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+  await expect(page.getByPlaceholder('Username')).toBeVisible();
 });
